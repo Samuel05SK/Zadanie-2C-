7ea1636a87c5c65840e39419dc7e3e9de7b0a713
samko <samko@MSI>
2026-04-10 08:53:09 +0200

    Add project files.

---
+++ Zadanie 2C++++/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Zadanie_2C____
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
