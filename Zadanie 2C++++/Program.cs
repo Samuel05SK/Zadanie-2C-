7ff15b4a571de5613ac69616fa51f63b8b1321c1
Ejdrijenn <adkoorel@gmail.com>
2026-04-10 10:24:06 +0200

    gg

--- Zadanie 2C++++/Program.cs
+++ Zadanie 2C++++/Program.cs
@@ -1,5 +1,15 @@
 ﻿namespace Zadanie_2C____
 {
+    public class Individuals
+    {
+
+    }
+
+    public class Population
+    {
+
+    }
+
     internal class Program
     {
         static void Main(string[] args)
