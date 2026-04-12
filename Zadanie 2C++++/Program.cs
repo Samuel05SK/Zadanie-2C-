1ba04077f1d544b6589c17b98abdbf4d41502da5
Ejdrijenn <adkoorel@gmail.com>
2026-04-12 09:13:09 +0200

    dddd

--- Zadanie 2C++++/Program.cs
+++ Zadanie 2C++++/Program.cs
@@ -18,6 +18,7 @@ namespace Zadanie_2C____
         public double Fitness(double x, double y)
         {
             double funkcna_hodnota = 0;
+            funkcna_hodnota = -Math.Cos(x) * Math.Sin(y) * Math.Exp(-(pow(x - Math.PI, 2)) + (pow(y - Math.PI, 2)));
             return funkcna_hodnota;
         }
     }
