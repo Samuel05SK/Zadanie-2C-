8551fd28cf7cb71d3b286dc5d7b286f5375d5901
Samko Skladany <samkoskladany321@gmail.com>
2026-04-12 13:37:42 +0200

    ijo.

--- Zadanie 2C++++/Program.cs
+++ Zadanie 2C++++/Program.cs
@@ -2,13 +2,13 @@
 {
     public class Individuals
     {
+        private Random random = new Random();
         public double x;
         public double y;
 
         public double Generator()
         {
             double hodnota;
-            Random random = new Random();
             hodnota = -10 + (random.NextDouble()) * 20;
             return hodnota;
         }
@@ -20,11 +20,10 @@
 
         public void mutate(double mutationRate)
         {
-            Random random = new Random();
             double hodnota = random.NextDouble();
             if(hodnota < mutationRate)
             {
-                int randomXY = random.Next(1,2);
+                int randomXY = random.Next(1,3);
                 switch(randomXY)
                 {
                     case 1:
@@ -39,69 +38,69 @@
 
     }
 
-    public class Population
-    {
-     
-        List<Individuals> jednotlivci = new List<Individuals>();
-
-        public int selection(double selectionRate)
-        {
-            jednotlivci.OrderByDescending(Individuals => Individuals.Fitness());
-            int dlzka = jednotlivci.Count;
-            int alive = (int)Math.Round(dlzka * selectionRate);
-            int pocetMrtvych = dlzka - alive;
-            jednotlivci.RemoveRange(alive, pocetMrtvych);
-            return pocetMrtvych;
-        }
-
-        public void cloning(double mutationRate)
-        {
-            int velkostPopulacie = jednotlivci.Count;
-            Random random = new Random();
-            int klonovanyJedinec = random.Next(0, velkostPopulacie - 2);
-            
-            jednotlivci.Add(new Individuals());
-            int Alex = velkostPopulacie; //Alex == klon
-            jednotlivci[Alex].x = jednotlivci[klonovanyJedinec].x;
-            jednotlivci[Alex].y = jednotlivci[klonovanyJedinec].y;
-            jednotlivci[Alex].mutate(mutationRate);
-        }
-
-        public double baseGen()
-        {
-            jednotlivci.OrderByDescending(Individuals => Individuals.Fitness());
-            return jednotlivci[0].Fitness();
-        }
-
-        public double nextGen(double selectionRate, double mutationRate)
-        {
-            int pocetMrtvych = selection(selectionRate);
-            for(int i = 0; i<pocetMrtvych; i++)
-            {
-                cloning(mutationRate);
-            }
-            return baseGen();
-        }
-
-        public Population(int maxPopulation)
-        {
-            for(int i = 0; i < maxPopulation; i++)
-            {
-                jednotlivci.Add(new Individuals());
-                jednotlivci[i].x = jednotlivci[i].Generator();
-                jednotlivci[i].y = jednotlivci[i].Generator();
-            }
-
-        }
-    }
+   
 
     internal class Program
     {
+        public static List<Individuals> jednotlivci = new List<Individuals>();
+        private class Population
+        {
+            private Random random = new Random();
+            
+            public int selection(double selectionRate)
+            {
+                jednotlivci = jednotlivci.OrderByDescending(ind => ind.Fitness()).ToList();
+                foreach (Individuals i in jednotlivci)
+                {
+                    Console.WriteLine("Fitness:    " + i.Fitness());
+                }
+                int dlzka = jednotlivci.Count;
+                int alive = (int)Math.Round(dlzka * selectionRate);
+                int pocetMrtvych = dlzka - alive;
+                jednotlivci.RemoveRange(alive, pocetMrtvych);
+                return pocetMrtvych;
+            }
+
+            public void cloning(double mutationRate)
+            {
+                int velkostPopulacie = jednotlivci.Count;
+                int klonovanyJedinec = random.Next(0, velkostPopulacie - 1);
+                jednotlivci.Add(new Individuals());
+                int Alex = velkostPopulacie; //Alex == klon
+                jednotlivci[Alex].x = jednotlivci[klonovanyJedinec].x;
+                jednotlivci[Alex].y = jednotlivci[klonovanyJedinec].y;
+                jednotlivci[Alex].mutate(mutationRate);
+            }
+
+            public double baseGen()
+            {
+                jednotlivci = jednotlivci.OrderByDescending(ind => ind.Fitness()).ToList();
+                return jednotlivci[0].Fitness();
+            }
+
+            public double nextGen(double selectionRate, double mutationRate)
+            {
+                int pocetMrtvych = selection(selectionRate);
+                for (int i = 0; i < pocetMrtvych; i++)
+                {
+                    cloning(mutationRate);
+                }
+                return baseGen();
+            }
+
+            public Population(int maxPopulation)
+            {
+                for (int i = 0; i < maxPopulation; i++)
+                {
+                    jednotlivci.Add(new Individuals());
+                    jednotlivci[i].x = jednotlivci[i].Generator();
+                    jednotlivci[i].y = jednotlivci[i].Generator();
+                }
+
+            }
+        }
         static void Main(string[] args)
         {
-
-            //Console.OutputEncoding = System.Text.Encoding.UTF8;
-
             int maxPopulation = 0;
             bool FunkciaOk = true;
             Console.WriteLine("Zadanie Variant F: Umelá krajina");
@@ -128,7 +127,7 @@
             Population populacia = new Population(maxPopulation);
             for(int i = 0;i < 100;i++)
             {
-                Console.WriteLine(populacia.nextGen(0.2, 0.02));
+                Console.WriteLine(populacia.nextGen(0.2, 0.2));
             }
 
             Console.ReadLine();
